f6d3c9f35c1cacc79f2d7f6524ecb95ca2634351
Drikus Roor <drikusroor@gmail.com>
2020-05-30 12:48:06 +0200

    Rename event to appointment

--- test/index.js
+++ test/index.js
@@ -1,47 +1,47 @@
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const moment = require("moment");
-const { planEvents } = require("../index");
+const { planAppointments } = require("../index");
 
 const clients = [
   {
     name: "John",
-    lastEvent: "2019-04-30",
+    lastAppointment: "2019-04-30",
     repeatAfter: { unit: "days", amount: 3 },
   },
   {
     name: "Paul",
-    lastEvent: "2020-03-20",
+    lastAppointment: "2020-03-20",
     repeatAfter: { unit: "days", amount: 2 },
   },
   {
     name: "George",
-    lastEvent: "2019-12-20",
+    lastAppointment: "2019-12-20",
     repeatAfter: { unit: "hours", amount: 3 },
     constraints: [{ type: "period", unit: "hours", min: 13, max: 18 }],
   },
   {
     name: "Ringo",
-    lastEvent: "2020-05-20",
+    lastAppointment: "2020-05-20",
     repeatAfter: { unit: "hours", amount: 4 },
     constraints: [{ type: "weekdays", values: [1, 2, 4, 5] }],
   },
   {
     name: "Brian",
-    lastEvent: "2020-05-29 11:00",
+    lastAppointment: "2020-05-29 11:00",
     repeatAfter: { unit: "hours", amount: 5 },
-    eventLength: { unit: "minutes", amount: 60 },
+    appointmentLength: { unit: "minutes", amount: 60 },
     constraints: [{ type: "period", unit: "hours", min: 8, max: 12 }],
   },
 ];
 
-const events = planEvents(clients);
+const appointments = planAppointments(clients);
 
-const mappedEvents = events.map((event) => ({
-  name: event.name,
-  startDate: moment(event.start).format("YYYY/MM/DD"),
-  startTime: moment(event.start).format("h:mm:ss a"),
-  endDate: moment(event.end).format("YYYY/MM/DD"),
-  endTime: moment(event.end).format("h:mm:ss a"),
+const mappedAppointments = appointments.map((appointment) => ({
+  name: appointment.name,
+  startDate: moment(appointment.start).format("YYYY/MM/DD"),
+  startTime: moment(appointment.start).format("h:mm:ss a"),
+  endDate: moment(appointment.end).format("YYYY/MM/DD"),
+  endTime: moment(appointment.end).format("h:mm:ss a"),
 }));
 
 const csvWriter = createCsvWriter({
@@ -55,4 +55,4 @@ const csvWriter = createCsvWriter({
   ],
 });
 
-csvWriter.writeRecords(mappedEvents);
+csvWriter.writeRecords(mappedAppointments);
